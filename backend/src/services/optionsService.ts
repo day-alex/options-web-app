41877cca3bee907db61c273785cb0538f4cdf0ba
Alex Day <dayjose3@msu.edu>
2025-05-18 23:57:05 -0400

    black scholes call price function

--- backend/src/services/optionsService.ts
+++ backend/src/services/optionsService.ts
@@ -1,4 +1,5 @@
 import { OptionInputData, OptionResultData, OptionCalculationResult } from '../models/optionTypes';
+import standardNormalCDF from '../utils/math';
 
 export class OptionsService {
   /**
@@ -29,7 +30,20 @@ export class OptionsService {
   }
   
   private calculateCallPrice(data: OptionInputData): number {
-    return 0;
+    const spot = data.spot;
+    const strike = data.strike;
+    const exp = data.exp;
+    const rate = data.rate;
+    const vol = data.vol
+
+    const d1 = (Math.log(spot/strike)+(rate+((vol**2)/2))*exp)/(vol*Math.sqrt(exp));
+    const d2 = d1 - (vol*Math.sqrt(exp));
+
+    const nd1 = standardNormalCDF(d1);
+    const nd2 = standardNormalCDF(d2);
+
+    const call = (spot*nd1) - (strike*(Math.E**(-rate*exp))*nd2);
+    return call;
   }
   
   private calculatePutPrice(data: OptionInputData): number {
